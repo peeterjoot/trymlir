a133d381a4ce312edc56276ba56786a0655d2aa3
Peeter Joot <peeterjoot@pm.me>
2025-04-17 08:33:51 -0400

    Fix ToyDialect.h: use the right defines/namespaces, remove dup includes.

--- src/ToyDialect.h
+++ src/ToyDialect.h
@@ -1,32 +1,20 @@
 #ifndef TOY_DIALECT_H
 #define TOY_DIALECT_H
 
-#include "mlir/Bytecode/BytecodeOpInterface.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/Dialect.h"
-#include "mlir/IR/Operation.h"
 #include "mlir/IR/OpDefinition.h"
-#include "mlir/Support/TypeID.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
 
-// Include generated dialect declarations
-#include "ToyDialectBase.h.inc"
-// Include generated type declarations
-#include "ToyDialectTypes.h.inc"
-// Include generated operation declarations
-#include "ToyDialect.h.inc"
-
-namespace toy {
-
-// Forward declaration of ToyDialect
-class ToyDialect;
-
-// Define types and operations using TableGen includes
-#define GET_OP_CLASSES
-#include "ToyDialect.h.inc"
-
+// Include the generated type declarations
 #define GET_TYPEDEF_CLASSES
 #include "ToyDialectTypes.h.inc"
 
-} // namespace toy
+// Include the generated dialect declarations
+#include "ToyDialectBase.h.inc"
+
+// Include the generated operation declarations
+#define GET_OP_CLASSES
+#include "ToyDialect.h.inc"
 
 #endif // TOY_DIALECT_H
